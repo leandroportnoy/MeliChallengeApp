78dcd2ee8914510ee3d3ca331a99f76a4acd6828
Leandro Alves <leandro@xsoluti.com.br>
2025-03-31 01:21:40 -0300

    creating some components e fixing some lib errors

--- app/src/main/java/br/com/las/melichallenge/MainActivity.kt
+++ app/src/main/java/br/com/las/melichallenge/MainActivity.kt
@@ -11,7 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.las.melichallenge.ui.theme.MeliChallengeTheme
+import br.com.las.ui.MeliChallengeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -1,23 +1,18 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.dagger.hilt)
 }
 
 android {
-    namespace = "br.com.las.melichallenge"
+    namespace = "br.com.las.ui"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "br.com.las.melichallenge"
-        minSdk = 29
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = 24
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -36,35 +31,22 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
-    implementation(project(":ui"))
-//    implementation(project(":core"))
-//    implementation(project(":data"))
-//    implementation(project(":features"))
-//    implementation(project(":common"))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation(libs.dagger.hilt)
-    ksp(libs.hilt.compiler)
-
+//    implementation(libs.compose.ui)
+//    implementation(libs.compose.ui.tooling)
 }
