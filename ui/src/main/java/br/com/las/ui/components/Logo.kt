d6bd31feaf6df52359abdd4174bcba0e6f221ce0
Leandro Alves <leandro@xsoluti.com.br>
2025-04-01 21:17:15 -0300

    more adjustments

--- ui/src/main/java/br/com/las/ui/components/Logo.kt
+++ ui/src/main/java/br/com/las/ui/components/Logo.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
